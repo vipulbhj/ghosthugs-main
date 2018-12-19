01ad46d892e90242689007b7b7021a790e0c9849
vipulbhj <2015pcecsvipul119@poornima.org>
2018-12-19 21:37:57 +0530

    Basic Layout Created;3pages added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Ghosthugs.com',
+    description: 'One place store for all your IT needs.',
+    author: '@vipulbhj',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
