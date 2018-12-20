542ae463ce418386edf84d05d53bfdc7b8c8c02c
vipulbhj <2015pcecsvipul119@poornima.org>
2018-12-20 17:03:54 +0530

    Slider Template Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,30 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import imgSrc from '../images/kobu-agency-798655-unsplash.jpg';
+
+console.log(imgSrc);
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['ghosthugs', 'home', 'IT', 'company']} />
-    <h3>HOME</h3>
+    <div className="home-container">
+      <div className="main-slider" style={{
+        backgroundImage: `url('${imgSrc}')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center left',
+        backgroundRepeat: 'no-repeat'
+      }}
+     >
+        <p>
+          helping companies with the ideal tech solution for there product.
+        </p>
+      </div>
+
+      <div className="content">B</div>
+      <div className="adds">C</div>
+      <div className="other-slider">D</div>
+    </div>
   </Layout>
 )
 
