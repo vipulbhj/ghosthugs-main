595ec6c817d8facd1e83d04303567b151fb5a306
vipulbhj <2015pcecsvipul119@poornima.org>
2018-12-21 20:52:47 +0530

    Home Page Completed

---
+++ src/components/tile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TILE = (props) => (
+    <div className="tile">
+        <p>{props.value}</p>
+    </div>
+)
+
+export default TILE;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,69 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import CARD from '../components/card';
+import Tile from '../components/tile';
 
 import ai from '../images/ai.png';
 import app from '../images/app.jpg';
 import analytics from '../images/analytics.png';
+import azure from '../images/azure.svg';
 import cover from '../images/cover.jpg';
 
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const workAreas = [
+    'Business Intelligence', 
+    'Data Analytics', 
+    'CI/CD', 
+    'DevOps',
+    'Logistics', 
+    'Azure', 
+    'AWS', 
+    'React JS', 
+    'Node JS',
+    'react native',
+    'web dev',
+    'performance optimization'
+  ]; 
+  return (
   <>
-  <SEO title="Home" keywords={['ghosthugs', 'home', 'IT', 'company']} />
-  <Layout>
-    <div className="home-page">
-      <div className="home-main-slider">
-        <h2>ghosthugs</h2>
-        <small>The Tech hussle is real, leave it to us and grow your comapany fastest.</small>
-      </div>
-      <section className="home-info">
-        <h3>What we do</h3>
-        <div className="slider">
-          <div className="slides">
-            <CARD src={ai} title="Machine Learning" des="We provide experties in the domain of ML and AI" />
-            <CARD src={app} title="App Development" des="Cross Platform or PWA, whatever suits your business better we have it" />
-            <CARD src={analytics} title="Business Analytics" des="Understand your audience better with data extracted facts" />
-            <CARD src={ai} title="Machine Learning" des="We provide experties in the domain of ML and AI" />
+    <SEO title="Home" keywords={['ghosthugs', 'home', 'IT', 'company']} />
+    <Layout>
+      <div className="home-page">
+        <div className="home-main-slider">
+          <h2>ghosthugs</h2>
+          <small>The Tech hussle is real, leave it to us and grow your comapany fastest.</small>
+        </div>
+        <section className="home-info">
+          <h3>What we do</h3>
+          <div className="slider">
+            <div className="slides">
+              <CARD src={ai} title="Machine Learning" des="We provide experties in the domain of ML and AI" />
+              <CARD src={azure} title="IaaS and Automation" des="DevOps powered by Microsoft Azure" />
+              <CARD src={app} title="App Development" des="Cross Platform or PWA, whatever suits your business better we have it" />
+              <CARD src={analytics} title="Business Analytics" des="Understand your audience better with data extracted facts" />
+              
+            </div>
           </div>
+        </section>
+        <aside className="home-meta-info">
+          <h3>We Specialize In</h3> 
+          {
+            workAreas.map((item,key) => {
+                return <Tile key={key} value={item} />
+            })
+          }
+        </aside>
+        <div className="home-second-slider" style={{
+          backgroundImage: `url('${cover}')`
+        }}>
+          <article>
+            <p>perfection is achieved not when there is nothing to add but when there is nothing to take away</p>
+          </article>
         </div>
-      </section>
-      <aside className="home-meta-info">
-        <h3 style={{marginBottom: '30px'}}>Why us</h3>
-        <div className="previous-work-showcase">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </aside>
-      <div className="home-second-slider" style={{
-        backgroundImage: `url('${cover}')`
-      }}>
-        <article>
-          <p>perfection is achieved not when there is nothing to add but when there is nothing to take away</p>
-        </article>
       </div>
-    </div>
-  </Layout>
+    </Layout>
   </>
 )
-
+}
 export default IndexPage;
