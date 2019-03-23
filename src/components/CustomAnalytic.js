b1af145469070bdecb4aec75b1f01b7192866884
vipulbhj <vipul@github.com>
2019-03-24 02:41:54 +0530

    Analytics Bug fixed

--- src/components/CustomAnalytic.js
+++ src/components/CustomAnalytic.js
@@ -6,14 +6,7 @@ class CustomAnalytic extends Component {
         this.startTime = 0;
     }
 
-    componentDidMount() {
-        this.startTime = new Date();
-    }
-
-    componentWillUnmount() {
-        let time = new Date() - this.startTime;
-        console.log('You spent ',time,' milli seconds on ', this.props.details.pageUrl, ' page');
-        
+    analytics(time, pageUrl) {
         // sending page data to analytics server
         // 'https://cryptic-refuge-46201.herokuapp.com/api/analytic'
         // http://localhost:3000/api/analytic
@@ -24,19 +17,43 @@ class CustomAnalytic extends Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                pageUrl: this.props.details.pageUrl,
+                pageUrl: pageUrl,
                 timeSpentOnThePage: time
             })
         })
         .then((res) => { return res.json(); })
         .then((data) => {console.log(JSON.stringify(data))});
     }
+
+    componentDidMount() {
+        this.startTime = new Date();
+        const startTimeTemp = new Date();
+        const pageUrl = this.props.details.pageUrl; 
+        const analyticsTemp = this.analytics;
+        window.addEventListener('beforeunload', (e) => {
+            // Cancel the event
+            e.preventDefault();
+            
+            const time = new Date() - startTimeTemp;
+            analyticsTemp(time, pageUrl);
+
+            // Chrome requires returnValue to be set
+            e.returnValue = '';
+
+            return;
+        });
+    }
+
+    componentWillUnmount() {
+        let time = new Date() - this.startTime;
+        console.log('You spent ',time,' milli seconds on ', this.props.details.pageUrl, ' page');
+        this.analytics(time, this.props.details.pageUrl);
+    }
     
     render() {
         return (
             <>
                 {this.props.render()}
-                {/* <BlogPage /> */}
             </>
         );
     } 
